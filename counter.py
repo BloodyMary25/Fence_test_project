daeb0d945f84131ca47c499533ed7f5cfcf651be
BloodyMary25 <novaabraxas@gmail.com>
2024-01-29 09:55:53 +0300

    немного навели порядок

--- counter.py
+++ counter.py
@@ -23,17 +23,6 @@ def main(page: ft.Page):
         txt_number.value = str(int(txt_number.value) + 1)
         page.update()
 
-    page.add(
-        ft.Row(
-            [
-                ft.Icon(name=ft.icons.MAN_2, color=ft.colors.RED, size=30),
-                ft.IconButton(ft.icons.REMOVE, on_click=minus_click_1, icon_color='red'),
-                txt_number,
-                ft.IconButton(ft.icons.ADD, on_click=plus_click_1, icon_color="red"),
-            ],
-            alignment=ft.MainAxisAlignment.CENTER,
-        )
-    )
 
     def minus_click_2(e):
         txt_number2.value = str(int(txt_number2.value) - 1)
@@ -52,6 +41,18 @@ def main(page: ft.Page):
                 txt_number2,
                 ft.IconButton(ft.icons.ADD, on_click=plus_click_2, icon_color='blue')
 
+            ],
+            alignment=ft.MainAxisAlignment.CENTER,
+
+        )
+    )
+    page.add(
+        ft.Row(
+            [
+                ft.Icon(name=ft.icons.MAN_2, color=ft.colors.RED, size=30),
+                ft.IconButton(ft.icons.REMOVE, on_click=minus_click_1, icon_color='red'),
+                txt_number,
+                ft.IconButton(ft.icons.ADD, on_click=plus_click_1, icon_color="red"),
             ],
             alignment=ft.MainAxisAlignment.CENTER,
         )
